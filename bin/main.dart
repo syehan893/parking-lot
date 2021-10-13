fb49c674ad10a6f85bde031cd959f74cc2bfd201
Muhammad Syehan <muhammad.syehan@aux.dkatalis.com>
2021-10-13 10:08:17 +0700

    [CHORES] init dart project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:parking_lot/parking_lot.dart' as parking_lot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${parking_lot.calculate()}!');
+}
---
+++ test/parking_lot_test.dart
@@ -0,0 +1,8 @@
+import 'package:parking_lot/parking_lot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
