3d98cde74aa58dad4bdf8179cfd9589a9c6640d8
Muhammad Syehan <muhammad.syehan@aux.dkatalis.com>
2021-10-13 10:18:03 +0700

    [TDD-RED] add test in vehicle model : should have registrationNumber and color

---
+++ test/models/vehicle_model_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('vehicle model test', () {
+    test('should have registrationNumber and color', () {
+      var registrationNumber = 'KA-1234-SS-04';
+      var color = 'blue';
+      var props = [registrationNumber, color];
+      final vehicle =
+          Vehicle(registrationNumber: registrationNumber, color: color);
+      expect(vehicle.props, props);
+    });
+  });
+}
