1c3bdca10f142eee3ece33b873c16066fc8a294e
Muhammad Syehan <muhammad.syehan@aux.dkatalis.com>
2021-10-13 11:19:58 +0700

    [TDD-GREEN] implement test in parking lot controller : should return success message when controller call create parking lot command

--- lib/common/constant/parking_lot_constant.dart
+++ lib/common/constant/parking_lot_constant.dart
@@ -1,4 +1,8 @@
 class ParkingLotConstant {
   static const createdParkingLot = 'Created parking lot with %s slots';
   static const emptySpace = ' ';
+  static const commonSuccess = 'Success';
+  static const invalidCommand = 'Invalid Command';
+  static const noCommentAreSelected = 'NO COMMANDS ARE SELECTED';
+  static const createParkingLot = 'create_parking_lot';
 }
--- lib/controller/parking_lot_controller.dart
+++ lib/controller/parking_lot_controller.dart
@@ -1,3 +1,5 @@
+import 'package:parking_lot/commands/create_parking_lot_command.dart';
+import 'package:parking_lot/common/constant/parking_lot_constant.dart';
 import 'package:parking_lot/services/parking_lot_service.dart';
 
 class ParkingLotController {
@@ -6,6 +8,20 @@ class ParkingLotController {
   ParkingLotController({this.parkingLotRepository});
 
   String parkingLotCommands(String command) {
-   return command;
+    var commandParkingLot = command.split(ParkingLotConstant.emptySpace)[0];
+
+    var createParkingLotCommand =
+        CreateParkingLotCommand(parkingLotRepository: parkingLotRepository);
+
+    switch (commandParkingLot) {
+      case ParkingLotConstant.createParkingLot:
+        final result = createParkingLotCommand.execute(command);
+        return result;
+        break;
+      default:
+        print(ParkingLotConstant.invalidCommand);
+        return ParkingLotConstant.noCommentAreSelected;
+        break;
+    }
   }
 }
