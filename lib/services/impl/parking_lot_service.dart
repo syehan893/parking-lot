c48b7818e2abd8bef3e8ba683d013d3fc91c8797
Muhammad Syehan <muhammad.syehan@aux.dkatalis.com>
2021-10-13 11:05:48 +0700

    [TDD-GREEN] implement test in parking lot service : should return success message when parking lot created

--- lib/services/impl/parking_lot_service.dart
+++ lib/services/impl/parking_lot_service.dart
@@ -1,6 +1,7 @@
 import 'package:parking_lot/common/constant/parking_lot_constant.dart';
 import 'package:parking_lot/services/parking_lot_service.dart';
 import 'package:parking_lot/models/parking_lot_model.dart';
+import 'package:parking_lot/models/slot_model.dart';
 import 'package:sprintf/sprintf.dart';
 
 class ParkingLotServiceImpl implements ParkingLotService {
@@ -8,6 +9,10 @@ class ParkingLotServiceImpl implements ParkingLotService {
 
   @override
   String createParkingLot(int capacity) {
+    parkingLot = parkingLot.copywith(
+        capacity: capacity,
+        slots: List.generate(capacity,
+            (index) => Slot(slotNumber: index + 1, vehicleParked: null)));
     return sprintf(ParkingLotConstant.createdParkingLot, [parkingLot.capacity]);
   }
 }
