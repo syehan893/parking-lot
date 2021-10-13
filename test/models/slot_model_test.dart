e14a3cd5b715ee82a533dd6ab9dbc11563776af1
Muhammad Syehan <muhammad.syehan@aux.dkatalis.com>
2021-10-13 10:26:33 +0700

    [TDD-RED] add test in slot model : should return slot copy with

--- test/models/slot_model_test.dart
+++ test/models/slot_model_test.dart
@@ -6,9 +6,15 @@ void main() {
     var vehicle = Vehicle(color: 'blue', registrationNumber: 'KA-1234-SS-04');
     var slotNumber = 5;
     var actual = Slot(slotNumber: slotNumber, vehicleParked: vehicle);
+    var expectedCopyWith = Slot(slotNumber: 10, vehicleParked: vehicle);
     test('should have slot number and vehicle', () {
       var props = [vehicle, slotNumber];
       expect(actual.props, props);
+    });
+
+    test('should return slot copy with', () {
+      final actualCopyWith = actual.copywith(slotNumber: 10);
+      expect(actualCopyWith, expectedCopyWith);
     });
   });
 }
