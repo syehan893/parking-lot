5e5e767133ce87590a22454bbf5d54c76c12c7f3
Muhammad Syehan <muhammad.syehan@aux.dkatalis.com>
2021-10-13 10:35:54 +0700

    [TDD-GREEN] implement test in parking lot model : should return parking lot copy with

--- lib/models/parking_lot_model.dart
+++ lib/models/parking_lot_model.dart
@@ -7,6 +7,15 @@ class ParkingLot extends Equatable {
 
   ParkingLot({this.slots, this.capacity});
 
+  ParkingLot copywith({
+    List<Slot> slots,
+    int capacity,
+  }) =>
+      ParkingLot(
+        slots: slots ?? this.slots,
+        capacity: capacity ?? this.capacity,
+      );
+
   @override
   List<Object> get props => [slots, capacity];
 }
