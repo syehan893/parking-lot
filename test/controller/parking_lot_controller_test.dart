8cb2f5a8bf7e6d312eba1d95481bf3b03b85cbd0
Muhammad Syehan <muhammad.syehan@aux.dkatalis.com>
2021-10-13 14:18:03 +0700

    [TDD-RED] add test in parking lot controller : should return success message when controller call park parking lot command

--- test/controller/parking_lot_controller_test.dart
+++ test/controller/parking_lot_controller_test.dart
@@ -1,5 +1,6 @@
 import 'package:mockito/mockito.dart';
 import 'package:parking_lot/commands/create_parking_lot_command.dart';
+import 'package:parking_lot/commands/park_parking_lot_command.dart';
 import 'package:parking_lot/common/constant/parking_lot_constant.dart';
 import 'package:parking_lot/controller/parking_lot_controller.dart';
 import 'package:parking_lot/services/parking_lot_service.dart';
@@ -11,12 +12,15 @@ class MockParkingLotService extends Mock implements ParkingLotService {}
 void main() {
   group('parking lot controller test', () {
     CreateParkingLotCommand createParkingLotCommand;
+    ParkParkingLotCommand parkParkingLotCommand;
     MockParkingLotService mockParkingLotService;
     ParkingLotController parkingLotController;
     setUpAll(() {
       mockParkingLotService = MockParkingLotService();
       createParkingLotCommand =
           CreateParkingLotCommand(parkingLotRepository: mockParkingLotService);
+      parkParkingLotCommand =
+          ParkParkingLotCommand(parkingLotRepository: mockParkingLotService);
       parkingLotController =
           ParkingLotController(parkingLotRepository: mockParkingLotService);
     });
@@ -26,6 +30,16 @@ void main() {
       var command = 'create_parking_lot 6';
       var expected = sprintf(ParkingLotConstant.createdParkingLot, [6]);
       when(createParkingLotCommand.execute(command))
+          .thenAnswer((realInvocation) => expected);
+      final actual = parkingLotController.parkingLotCommands(command);
+      expect(actual, expected);
+    });
+    test(
+        'should return success message when controller call park parking lot command',
+        () {
+      var command = 'park KA-1234-SS';
+      var expected = sprintf(ParkingLotConstant.parkingSuccess, [1]);
+      when(parkParkingLotCommand.execute(command))
           .thenAnswer((realInvocation) => expected);
       final actual = parkingLotController.parkingLotCommands(command);
       expect(actual, expected);
