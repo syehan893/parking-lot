15e49af95974d476407007be12efddefaebe519e
Muhammad Syehan <muhammad.syehan@aux.dkatalis.com>
2021-10-13 10:47:11 +0700

    [TDD-REFACTOR] fix naming convention

--- test/models/request/leave_request_model_test.dart
+++ test/models/request/leave_request_model_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:parking_lot/models/request/leave_request_model.dart';
 void main() {
   group('leave response model test', () {
-    test('should have registrationNumber, parkingCharge and slotNumber', () {
+    test('should have registrationNumber, parkingCharge', () {
       var registrationNumber = 'KA-1234-SS-04';
       var parkingCharge = 5;
       var props = [registrationNumber, parkingCharge];
